294540fa5b7f8a21d7e5f78c6db90746f0df9bac
mohtar25 <mokhtar2000naj@gmail.com>
2024-10-07 12:20:51 +0200

    fixing trunclating table error

--- src/database/seeding.ts
+++ src/database/seeding.ts
@@ -1,9 +1,11 @@
 import type { Table } from "drizzle-orm";
 import { NodePgDatabase } from "drizzle-orm/node-postgres";
-import * as schema from "./schemas/";
+//import * as schema from "./schemas/";
 import { getTableName, sql } from "drizzle-orm";
 
-type schemaType = typeof schema;
-export async function turbTable(db: NodePgDatabase<schemaType>, table: Table) {
-  await db.execute(sql.raw(`DROP DATABASE IF EXISTS test_network:users_test `));
+//type schemaType = typeof schema;
+export async function turbTable(db: NodePgDatabase, table: Table) {
+  await db.execute(
+    sql.raw(`TRUNCATE TABLE "${getTableName(table)}" RESTART IDENTITY CASCADE`),
+  );
 }
